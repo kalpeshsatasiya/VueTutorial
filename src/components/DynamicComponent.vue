ede01e75d6b309d7ac156d297a92fe87e2ae8076
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2018-05-20 14:45:34 +0530

    Code refectoring of dynamic compnent

--- src/components/DynamicComponent.vue
+++ src/components/DynamicComponent.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="dynamic-component-demo">
-  <button
+  <button 
     v-for="tab in tabs"
     v-bind:key="tab"
     v-bind:class="['tab-button', { active: currentTab === tab }]"
-    v-on:click="currentTab = tab"
-  >{{ tab }}</button>
+    v-on:click="currentTab = tab">{{ tab }}</button>
 
-  
     <component
       v-bind:is="currentTabComponent"
-      class="tab"
-    ></component>
-  
+      class="tab"></component>  
 </div>
 </template>
-
 <script>
 import Vue from "vue";
-Vue.component("tab-demo1", {
-  template: "<div>Demo 1 component</div>"
-});
-
-Vue.component("tab-demo2", {
-  template: "<div>Demo 2 component</div>"
-});
-
-import Todos from "@/components/Todos";
 export default {
   name: "DynamicComponent",
   data() {
@@ -41,4 +27,12 @@ export default {
     }
   }
 };
+
+Vue.component("tab-demo1", {
+  template: "<div>Demo 1 component</div>"
+});
+
+Vue.component("tab-demo2", {
+  template: "<div>Demo 2 component</div>"
+});
 </script>
